0cb21c38463766e2f7d0b8aa759f0e812ba5cf44
Tollef Jørgensen <tollefj@gmail.com>
2018-03-10 12:40:09 +0100

    syntax update

--- iptrack.py
+++ iptrack.py
@@ -19,27 +19,29 @@
 # descending powers.
 
 import numpy as np
-import matplotlib as plot
 import os
 
 _file = os.getcwd() + "/rapport/run.txt"
 
-def iptrack(filename):
-	data=np.loadtxt(filename,skiprows=2)
-	return np.polyfit(data[:,1],data[:,2],15)
 
-def trvalues(p,x):
-	y=np.polyval(p,x)
-	dp=np.polyder(p)
-	dydx=np.polyval(dp,x)
-	ddp=np.polyder(dp)
-	d2ydx2=np.polyval(ddp,x)
-	alpha=np.arctan(-dydx)
-	R=(1.0+dydx**2)**1.5/d2ydx2
-	return [y,dydx,d2ydx2,alpha,R]
+def iptrack(filename):
+    data = np.loadtxt(filename, skiprows=2)
+    return np.polyfit(data[:, 1], data[:, 2], 15)
+
+
+def trvalues(p, x):
+    y = np.polyval(p, x)
+    dp = np.polyder(p)
+    dydx = np.polyval(dp, x)
+    ddp = np.polyder(dp)
+    d2ydx2 = np.polyval(ddp, x)
+    alpha = np.arctan(-dydx)
+    R = (1.0+dydx**2)**1.5/d2ydx2
+    return [y, dydx, d2ydx2, alpha, R]
+
 
 if __name__ == "__main__":
-	poly = iptrack(_file)
-	#print(poly)
-	#print(trvalues(poly, 0.01229306768))
-	#print(trvalues(poly, 0.5))
+    poly = iptrack(_file)
+    # print(poly)
+    #print(trvalues(poly, 0.01229306768))
+    #print(trvalues(poly, 0.5))
