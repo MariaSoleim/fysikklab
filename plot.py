6cdfb4c0c383b7a4c15b6f925fce7b545396ce90
Tollef Jørgensen <tollefj@gmail.com>
2018-03-10 12:37:05 +0100

    data/plots from xls files

--- plot.py
+++ plot.py
@@ -1,28 +1,50 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import openpyxl
+import os
+from openpyxl import Workbook, load_workbook
 
-from openpyxl import Workbook
+
+class Sheet:
+    def __init__(self, sheet):
+        self.sheet = sheet
+        self.t = self.col('A')  # time
+        self.x = self.col('B')  # x-position
+        self.y = self.col('C')  # y-position
+
+    def plot(self):
+        plt.plot(self.t, self.y)
+
+    def col(self, col_name):
+        return [c.value for c in self.sheet[col_name]]
+
+    def get_t(self):
+        return self.t
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+
 wb = Workbook()
-book = openpyxl.load_workbook('/rapport/lab2_linear0.xlsx')
-ws = wb.active
-sheet = book.active
+data_src = os.path.join(os.getcwd(), "lab2_data")
+linear_book, sine_book, steep_book = [], [], []
+data_dict = {"Linear": [], "Sine": [], "Steep": []}
 
-t = sheet['A']
-x = sheet['B']
-y = sheet['C']
+for curve_type in os.listdir(data_src):
+    _path = os.path.join(data_src, curve_type)
+    if "xlsx" not in _path:
+        continue
+    book = load_workbook(_path)
+    for sheet in book.sheetnames:
+        data_dict[curve_type[:-5]].append(Sheet(book[sheet]))
 
-new_t = []
-for item in t:
-    new_t.append(item.value)
-
-new_x = []
-for item in x:
-    new_x.append(item.value)
-
-new_y = []
-for item in y:
-    new_y.append(item.value)
-
-plt.plot(new_x, new_y)
-plt.show()
+for k, v in data_dict.items():
+    print(k)
+    fig_num = 0
+    for i in v:
+        plt.subplot(5, 2, fig_num+1)
+        i.plot()
+        fig_num += 1
+        print(i)
+    plt.show()
