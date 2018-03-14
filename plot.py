a1633e1c598e44457c8cba68a5ae60529d79f2c5
Tollef Jørgensen <tollefj@gmail.com>
2018-03-14 16:24:14 +0100

    fuck these plots 😡

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import os
 import math
@@ -10,6 +11,20 @@ m = 0.0027  # Fyll inn riktig masse
 r = 0.02  # Fyll inn riktig radius
 h = 0.01
 inertia = 2/3*m*r**2
+
+
+def plot_color(col, label):
+    return mpatches.Patch(color=col, label=label)
+
+
+Linear = plot_color('blue', 'Linear')
+Sine = plot_color('red', 'Sine')
+Steep = plot_color('green', 'Steep')
+
+
+_colors = {'Linear': 'r--',
+           'Sine': 'b-',
+           'Steep': 'g-.'}
 
 
 def euler(v, alpha):
@@ -41,35 +56,36 @@ class Sheet:
         # set up the sheets polynomial fit
         self.poly = self.iptrack()
 
+        # compute the acceleration at each point
+        self.acc = [self.compute_acceleration(x) for x in self.x]
+        self.friction = [self.compute_friction(x) for x in self.x]
+        self.velocity = []
+
         # for use by eulers method, initialize default values
-        self.velocity = [0]
-        self.distance = [self.x[0]]
-        self.acceleration = [self.compute_acceleration()]
-
-        # initialize the first acceleration value
-
-    def plot_x(self): plt.plot(self.t, self.x)
-
-    def plot_y(self): plt.plot(self.t, self.y)
-
-    def plot_euler(self):
-        # actual values
-        plt.plot(self.t, self.x)
-        # numerically computed values
-        plt.plot(self.t, self.distance)
+        self.euler_velocity = [0]
+        self.euler_dist = [self.x[0]]
+        self.euler_acc = [self.compute_acceleration()]
 
     def col(self, col_name):
         return [float(c.value) for c in self.sheet[col_name]
                 if c.value is not None]
 
-    def get_t(self): return self.t
-
-    def get_x(self): return self.x
-
-    def get_y(self): return self.y
-
     def iptrack(self):
         return np.polyfit(self.x, self.y, 15)
+
+    def compute_friction(self, point=None):
+        if point is None:
+            # choose the first point as default initial value
+            point = self.x[0]
+        alpha = trvalues(self.poly, point)[3]
+        return m * g * math.sin(alpha) + m * self.compute_acceleration(point)
+
+    # return interpolated points by euler
+    def get_euler_dist(self): return self.euler_dist
+
+    def get_euler_acc(self): return self.euler_acc
+
+    def get_euler_velocity(self): return self.euler_velocity
 
     def compute_acceleration(self, point=None):
         if point is None:
@@ -80,17 +96,21 @@ class Sheet:
 
     def euler_iteration(self):
         # fetch the first items in the experimental data sets
-        x = self.distance[-1]
-        v = self.velocity[-1]
+        x = self.euler_dist[-1]
+        v = self.euler_velocity[-1]
         alpha = get_alpha(self.poly, x)
 
+        # only estimate the next point if the x value
+        # does not exceed the real limit
         x_next = x + h*v
+        if x_next > max(self.x):
+            x_next = x
         v_next = v + h*g*math.sin(alpha)/(1 + (inertia/m*r**2))
         a_next = self.compute_acceleration(x_next)
 
-        self.distance.append(x_next)
-        self.velocity.append(v_next)
-        self.acceleration.append(a_next)
+        self.euler_dist.append(x_next)
+        self.euler_velocity.append(v_next)
+        self.euler_acc.append(a_next)
 
     def compute_euler(self):
         for i in range(len(self.t)-1):
@@ -109,43 +129,113 @@ def init():
         if "xlsx" not in _path:
             continue
         book = load_workbook(_path)
+        sheets_found = 0
         for sheet in book.sheetnames:
+            sheets_found += 1
             data_dict[curve_type[:-5]].append(Sheet(book[sheet]))
+        print("found " + str(sheets_found) + " sheets!")
 
 
 init()
 
-
-def plot_sheet(s):
-    print(s)
-    # iterate over every sheet/tab in the xls
-    poly = s.iptrack()
-    x_vals = [x for x in s.get_x() if x is not None]
-    print(x_vals)
-    #  accel = [trvalues(poly, x) for x in x_vals]
-    accel = []
-    for x in x_vals:
-        if x is not None:
-            alpha = trvalues(poly, x)
-            accel.append(g*math.sin(alpha)/(1+inertia/(m*r**2)))
-    s.set_acc(accel)
-    plt.subplot(5, 2, fig_num+1)
-    s.plot_acc()
-    s.plot_y()
-
+#  max_x_axis_size = -1
+x_axes = []
+y_axes = []
 
 for k, v in data_dict.items():
     # iterate over every xls document
-    print(k)
     fig_num = 0
+    euler_vals = {}  # a list of every point from each list (10)
+    real_vals = {}  # decide what metrics to use here, dist/velo/acc
+    # for each sheet!
     for s in v:
-        plt.subplot(5, 2, fig_num+1)
+        # find the max distance traveled
         s.compute_euler()
-        s.plot_euler()
         fig_num += 1
-    plt.show()
-    # do something with s here
-    #  plt.title(k)
-    #  plt.ylabel('acceleration m/s^2')
-    #  plt.xlabel('time')
-    plt.show()
+
+        # add values to the dictionary of lists
+        current_time = 0
+        #  for point in s.get_euler_acc():
+        #      try:
+        #          euler_vals[current_time].append(point)
+        #      except KeyError:
+        #          euler_vals[current_time] = []
+        #      current_time += 1
+        #  current_time = 0
+        for point in s.friction:  # plot friction per distance
+            try:
+                real_vals[current_time].append(point)
+            except KeyError:
+                real_vals[current_time] = []
+            current_time += 1
+    invalid_euler_keys = []
+    for w, e in euler_vals.items():
+        if len(e) < 9:
+            invalid_euler_keys.append(w)
+
+    invalid_real_keys = []
+    for w, e in real_vals.items():
+        if len(e) < 9:
+            invalid_real_keys.append(w)
+
+    for key in invalid_euler_keys:
+        euler_vals.pop(key, None)
+
+    for key in invalid_real_keys:
+        real_vals.pop(key, None)
+
+    euler_y_axis = []
+    euler_y_axis_err = []
+    y_axis = []
+    y_axis_err = []
+    x_axis = []  # x-axis
+    for time, data in real_vals.items():
+        x_data, y_data = data
+        # do something with the data here!
+        x_axis.append(x_data)
+        variance = np.var(y_data)
+        std = np.std(y_data)
+        avg = np.mean(y_data)
+        y_axis.append(avg)
+        y_axis_err.append(variance)
+
+    #  euler_frames = []
+    #  for time, data in euler_vals.items():
+    #      # do something with the data here!
+    #      euler_frames.append(time)
+    #      variance = np.var(data)
+    #      std = np.std(data)
+    #      avg = np.mean(data)
+    #      euler_y_axis.append(avg)
+    #      euler_y_axis_err.append(variance)
+    #      print("var: " + str(variance) + ", std: " + str(std) + ", avg: " + str(avg))
+    #  y_axis = y_axis[:len(x_axis)]
+    #  y_axis_err = y_axis_err[:len(x_axis)]
+    #  plt.plot(x_axis, y_axis)
+    #  plt.plot(x_axis, euler_y_axis)
+    #  plt.errorbar(x_axis, euler_y_axis, yerr=euler_y_axis_err, fmt="-")
+    #  _color = _colors[k]
+    #  plt.errorbar(x_axis, y_axis, yerr=y_axis_err, fmt=_color)
+
+    x_axes.append(len(x_axis))
+    y_axes.append(y_axis)
+    plt.plot(x_axis, y_axis)
+
+#  longest_x_len = max(x_axes)
+x = np.arange(0, max(x_axes)+1, 1)
+#  print('longest x: ' + str(max(x_axes)))
+for y in y_axes:  # iterate data points
+    new_y = y
+    while len(new_y) <= max(x_axes):
+        print('xd')
+        new_y.append(0)
+_color = _colors[k]
+plt.errorbar(x, new_y, yerr=y_axis_err, fmt=_color)
+
+
+# do something with s here
+plt.legend(handles=[Linear, Sine, Steep])
+plt.title("friction")
+plt.ylabel('friction (N)')
+plt.xlabel('x_axis (100 fps)')
+plt.show()
